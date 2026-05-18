8decbc6cc9d06c47e53c9d6d217e732087d531bc
Oscar Söderlund <odsod@users.noreply.github.com>
2026-05-18 09:10:18 +0200

    refactor: use connect error codes instead of custom ResponseError type
    - Replace custom ResponseError struct with connect.NewError() + httpStatusToConnectCode()
    - Add error_test.go with error code mapping coverage via httptest

--- error.go
+++ error.go
@@ -1,34 +1,51 @@
 package mapon
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
-)
 
-// ResponseError represents a VW Fleet Interface response error.
-type ResponseError struct {
-	// StatusCode is the HTTP status code of the response.
-	StatusCode int
-	// Body is the body of the response.
-	Body []byte
-}
+	"connectrpc.com/connect"
+)
 
 func newResponseError(httpResponse *http.Response) error {
 	body, err := io.ReadAll(httpResponse.Body)
 	if err != nil {
 		body = fmt.Appendf(nil, "failed to read response body: %s", err)
 	}
-	return &ResponseError{
-		StatusCode: httpResponse.StatusCode,
-		Body:       body,
+	var msg string
+	if len(body) > 0 {
+		msg = fmt.Sprintf("http %d: %s", httpResponse.StatusCode, body)
+	} else {
+		msg = fmt.Sprintf("http %d", httpResponse.StatusCode)
 	}
+	return connect.NewError(httpStatusToConnectCode(httpResponse.StatusCode), errors.New(msg))
 }
 
-// Error implements the error interface.
-func (e *ResponseError) Error() string {
-	if len(e.Body) > 0 {
-		return fmt.Sprintf("http %d: %s", e.StatusCode, string(e.Body))
+func httpStatusToConnectCode(statusCode int) connect.Code {
+	switch statusCode {
+	case http.StatusBadRequest:
+		return connect.CodeInvalidArgument
+	case http.StatusUnauthorized:
+		return connect.CodeUnauthenticated
+	case http.StatusForbidden:
+		return connect.CodePermissionDenied
+	case http.StatusNotFound:
+		return connect.CodeNotFound
+	case http.StatusConflict:
+		return connect.CodeAlreadyExists
+	case http.StatusTooManyRequests:
+		return connect.CodeResourceExhausted
+	case http.StatusNotImplemented:
+		return connect.CodeUnimplemented
+	case http.StatusServiceUnavailable:
+		return connect.CodeUnavailable
+	case http.StatusGatewayTimeout:
+		return connect.CodeDeadlineExceeded
+	case http.StatusInternalServerError:
+		return connect.CodeInternal
+	default:
+		return connect.CodeUnknown
 	}
-	return fmt.Sprintf("http %d", e.StatusCode)
 }
